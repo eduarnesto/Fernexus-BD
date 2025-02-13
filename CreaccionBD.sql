f0aa78586b8235c57dfa9ca9f16e02f74a6ceed6
dvi26 <145675318+dvi26@users.noreply.github.com>
2025-02-13 14:24:17 +0100

    COMMIT ARREGLO MAIN

---
+++ CreaccionBD.sql
@@ -0,0 +1,38 @@
+<<<<<<< Updated upstream
+=======
+CREATE TABLE ProductosCategorias (
+    IdCategoria INT,
+    IdProducto INT,
+    CONSTRAINT PK_ProductosCategorias PRIMARY KEY (IdCategoria, IdProducto),
+    CONSTRAINT FK_ProductosCategorias_Categoria FOREIGN KEY (IdCategoria) 
+        REFERENCES Categorias(IdCategoria),
+    CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) 
+        REFERENCES Productos(IdProducto)
+);
+
+CREATE TABLE Pedidos (
+    IdPedido INT PRIMARY KEY,
+    FechaPedido DateTime,
+    Coste DECIMAL(10,2)
+);
+
+CREATE TABLE PedidosProductos (
+    IdPedido INT,
+    IdProducto INT,
+    Cantidad INT,
+    PRIMARY KEY (IdPedido, IdProducto),
+	CONSTRAINT FK_PedidosProductos_Pedido FOREIGN KEY (IdPedido) REFERENCES Pedidos(IdPedido),
+    CONSTRAINT FK_PedidosProductos_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto) 
+);CREATE TABLE Categorias (
+    IdCategoria INT PRIMARY KEY IDENTITY(1,1),
+    nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Productos (
+    IdProducto INT PRIMARY KEY IDENTITY(1,1),
+    nombre VARCHAR(255) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    IdCategoria INT,
+    CONSTRAINT FK_Productos_Categoria FOREIGN KEY (idCategoria)  REFERENCES Categorias(idCategoria)
+);
+>>>>>>> Stashed changes
