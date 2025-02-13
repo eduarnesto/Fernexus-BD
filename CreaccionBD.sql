74f0b68664281ccba5afef33bd008719cdbc718b
eduarnesto <145675429+eduarnesto@users.noreply.github.com>
2025-02-13 14:30:47 +0100

    Cambios en el orden de las tablas

--- CreaccionBD.sql
+++ CreaccionBD.sql
@@ -1,12 +1,3 @@
-CREATE TABLE ProductosCategorias (
-    IdCategoria INT,
-    IdProducto INT,
-    CONSTRAINT PK_ProductosCategorias PRIMARY KEY (IdCategoria, IdProducto),
-    CONSTRAINT FK_ProductosCategorias_Categoria FOREIGN KEY (IdCategoria) 
-        REFERENCES Categorias(IdCategoria),
-    CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) 
-        REFERENCES Productos(IdProducto)
-=======
 CREATE TABLE Productos (
     idProducto INT IDENTITY(1,1),
     nombre VARCHAR(255) NOT NULL,
