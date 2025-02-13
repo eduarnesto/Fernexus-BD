2e0b896c00ca2e1c59e604f05db03cd2778c5146
eduarnesto <145675429+eduarnesto@users.noreply.github.com>
2025-02-13 14:28:14 +0100

    Cambios en la nomenclatura

--- CreaccionBD.sql
+++ CreaccionBD.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 CREATE TABLE ProductosCategorias (
     IdCategoria INT,
     IdProducto INT,
@@ -6,4 +7,42 @@ CREATE TABLE ProductosCategorias (
         REFERENCES Categorias(IdCategoria),
     CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) 
         REFERENCES Productos(IdProducto)
+=======
+CREATE TABLE Productos (
+    idProducto INT IDENTITY(1,1),
+    nombre VARCHAR(255) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    idCategoria INT,
+	CONSTRAINT PK_Productos PRIMARY KEY (IdPedido),
+);
+
+CREATE TABLE Pedidos (
+    IdPedido INT IDENTITY(1,1),
+    FechaPedido DateTime NOT NULL,
+    Coste DECIMAL(10,2),
+	CONSTRAINT PK_Pedidos PRIMARY KEY (IdPedido)
+);
+
+CREATE TABLE Categorias (
+    IdCategoria INT IDENTITY(1,1),
+    nombre VARCHAR(255) NOT NULL,
+	CONSTRAINT PK_Pedidos PRIMARY KEY (IdCategoria)
+);
+
+CREATE TABLE PedidosProductos (
+    IdPedido INT,
+    IdProducto INT,
+    Cantidad INT NOT NULL,
+    CONSTRAINT PK_Pedidos PRIMARY KEY (IdPedido, IdProducto),
+	CONSTRAINT FK_PedidosProductos_Pedido FOREIGN KEY (IdPedido) REFERENCES Pedidos(IdPedido),
+    CONSTRAINT FK_PedidosProductos_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto) 
+);
+
+CREATE TABLE ProductosCategorias (
+    IdCategoria INT,
+    IdProducto INT,
+    CONSTRAINT PK_ProductosCategorias PRIMARY KEY (IdCategoria, IdProducto),
+    CONSTRAINT FK_ProductosCategorias_Categoria FOREIGN KEY (IdCategoria) REFERENCES Categorias(IdCategoria),
+    CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto)
+>>>>>>> Stashed changes
 );
