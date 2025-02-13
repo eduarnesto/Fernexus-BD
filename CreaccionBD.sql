710151dcd0e8d3d587d86828f273d725ed830410
Marco55H <145675712+Marco55H@users.noreply.github.com>
2025-02-13 14:07:05 +0100

    Creacion PedidosProductos, pedidos

--- CreaccionBD.sql
+++ CreaccionBD.sql
@@ -7,3 +7,18 @@ CREATE TABLE ProductosCategorias (
     CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) 
         REFERENCES Productos(IdProducto)
 );
+
+CREATE TABLE Pedidos (
+    IdPedido INT PRIMARY KEY,
+    FechaPedido DateTime,
+    Coste DECIMAL(10,2)
+);
+
+CREATE TABLE PedidosProductos (
+    IdPedido INT,
+    IdProducto INT,
+    Cantidad INT,
+    PRIMARY KEY (IdPedido, IdProducto),
+	CONSTRAINT FK_PedidosProductos_Pedido FOREIGN KEY (IdPedido) REFERENCES Pedidos(IdPedido),
+    CONSTRAINT FK_PedidosProductos_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto) 
+);
