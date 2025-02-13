5d69ac34686309b0fc60c506e7f8dc6d3444118c
Marco55H <145675712+Marco55H@users.noreply.github.com>
2025-02-13 14:08:09 +0100

    Delete SqlTablasMarco.sql

--- SqlTablasMarco.sql
+++
@@ -1,20 +0,0 @@
-CREATE TABLE Pedidos (
-    IdPedido INT PRIMARY KEY,
-    FechaPedido DateTime,
-    Coste DECIMAL(10,2)
-);
-
-CREATE TABLE PedidosProductos (
-    IdPedido INT,
-    IdProducto INT,
-    Cantidad INT,
-    PRIMARY KEY (IdPedido, IdProducto),
-	CONSTRAINT FKPedidosProductosPedido FOREIGN KEY (IdPedido) REFERENCES Pedidos(IdPedido),
-    CONSTRAINT FKPedidosProductosProducto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto) 
-);CREATE TABLE ProductosCategorias (
-    IdCategoria INT,
-    IdProducto INT,
-    CONSTRAINT PKProductosCategorias PRIMARY KEY (IdCategoria, IdProducto),
-    CONSTRAINT FKProductosCategoriasCategoria FOREIGN KEY (IdCategoria) REFERENCES Categorias(IdCategoria),
-    CONSTRAINT FKProductosCategoriasProducto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto)
-);
