998d19c91935ca6437f44a844adb8e5e737d5f7a
dvi26 <145675318+dvi26@users.noreply.github.com>
2025-02-13 14:33:14 +0100

    Update CreaccionBD.sql

--- CreaccionBD.sql
+++ CreaccionBD.sql
@@ -1,41 +1,37 @@
-
-
-
-CREATE TABLE Pedidos (
-    IdPedido INT PRIMARY KEY,
-    FechaPedido DateTime,
-    Coste DECIMAL(10,2)
-);
-
-CREATE TABLE Categorias (
-    IdCategoria INT PRIMARY KEY IDENTITY(1,1),
-    nombre VARCHAR(255) NOT NULL
-);
-
-
-
 CREATE TABLE Productos (
-    IdProducto INT PRIMARY KEY IDENTITY(1,1),
+    idProducto INT IDENTITY(1,1),
     nombre VARCHAR(255) NOT NULL,
     precio DECIMAL(10,2) NOT NULL,
-    IdCategoria INT,
-    CONSTRAINT FK_Productos_Categoria FOREIGN KEY (idCategoria)  REFERENCES Categorias(idCategoria)
-);
-CREATE TABLE ProductosCategorias (
-    IdCategoria INT,
-    IdProducto INT,
-    CONSTRAINT PK_ProductosCategorias PRIMARY KEY (IdCategoria, IdProducto),
-    CONSTRAINT FK_ProductosCategorias_Categoria FOREIGN KEY (IdCategoria) 
-        REFERENCES Categorias(IdCategoria),
-    CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) 
-        REFERENCES Productos(IdProducto)
+    idCategoria INT,
+	CONSTRAINT PK_Productos PRIMARY KEY (IdPedido),
+);
+
+CREATE TABLE Pedidos (
+    IdPedido INT IDENTITY(1,1),
+    FechaPedido DateTime NOT NULL,
+    Coste DECIMAL(10,2),
+	CONSTRAINT PK_Pedidos PRIMARY KEY (IdPedido)
+);
+
+CREATE TABLE Categorias (
+    IdCategoria INT IDENTITY(1,1),
+    nombre VARCHAR(255) NOT NULL,
+	CONSTRAINT PK_Pedidos PRIMARY KEY (IdCategoria)
 );
 
 CREATE TABLE PedidosProductos (
     IdPedido INT,
     IdProducto INT,
-    Cantidad INT,
-    PRIMARY KEY (IdPedido, IdProducto),
+    Cantidad INT NOT NULL,
+    CONSTRAINT PK_Pedidos PRIMARY KEY (IdPedido, IdProducto),
 	CONSTRAINT FK_PedidosProductos_Pedido FOREIGN KEY (IdPedido) REFERENCES Pedidos(IdPedido),
     CONSTRAINT FK_PedidosProductos_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto) 
+);
+
+CREATE TABLE ProductosCategorias (
+    IdCategoria INT,
+    IdProducto INT,
+    CONSTRAINT PK_ProductosCategorias PRIMARY KEY (IdCategoria, IdProducto),
+    CONSTRAINT FK_ProductosCategorias_Categoria FOREIGN KEY (IdCategoria) REFERENCES Categorias(IdCategoria),
+    CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto)
 );
