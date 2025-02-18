63a1196106a04748253d51fc6d6f47a4e633ff42
Marco55H <145675712+Marco55H@users.noreply.github.com>
2025-02-18 13:35:35 +0100

    Inserts Productos Categorias y Proveedores

--- CreaccionBD.sql
+++ CreaccionBD.sql
@@ -1,66 +1,27 @@
-Create database Fernexus
-Use Fernexus
+CREATE TABLE ProductosCategorias (
+    IdCategoria INT,
+    IdProducto INT,
+    CONSTRAINT PK_ProductosCategorias PRIMARY KEY (IdCategoria, IdProducto),
+    CONSTRAINT FK_ProductosCategorias_Categoria FOREIGN KEY (IdCategoria) 
+        REFERENCES Categorias(IdCategoria),
+    CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) 
+        REFERENCES Productos(IdProducto)
+);
 
 CREATE TABLE Pedidos (
-    IdPedido INT IDENTITY(1,1),
-    FechaPedido DateTime NOT NULL,
-    Coste DECIMAL(10,2),
-	CONSTRAINT PK_Pedidos PRIMARY KEY (IdPedido)
-);
-
-CREATE TABLE Productos (
-    IdProducto INT IDENTITY(1,1),
-    Nombre VARCHAR(255) NOT NULL,
-    Precio DECIMAL(10,2) NOT NULL,
-    IdCategoria INT,
-	CONSTRAINT PK_Productos PRIMARY KEY (IdProducto),
-);
-
-
-CREATE TABLE Proveedores (
-    IdProveedor INT IDENTITY(1,1),
-    Nombre VARCHAR (255) NOT NULL,
-    Correo VARCHAR (255) UNIQUE NOT NULL,
-    Telefono VARCHAR (20) NOT NULL,
-    Direccion TEXT NOT NULL,
-    Pais VARCHAR (100) NOT NULL
-	CONSTRAINT PK_Proveedores PRIMARY KEY (IdProveedor)
-);
-
-CREATE TABLE Categorias (
-    IdCategoria INT IDENTITY(1,1),
-    nombre VARCHAR(255) NOT NULL,
-	CONSTRAINT PK_Categorias PRIMARY KEY (IdCategoria)
+    IdPedido INT PRIMARY KEY,
+    FechaPedido DateTime,
+    Coste DECIMAL(10,2)
 );
 
 CREATE TABLE PedidosProductos (
     IdPedido INT,
     IdProducto INT,
-    Cantidad INT NOT NULL,
-    CONSTRAINT PK_PedidosProductos PRIMARY KEY (IdPedido, IdProducto),
+    Cantidad INT,
+    PRIMARY KEY (IdPedido, IdProducto),
 	CONSTRAINT FK_PedidosProductos_Pedido FOREIGN KEY (IdPedido) REFERENCES Pedidos(IdPedido),
     CONSTRAINT FK_PedidosProductos_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto) 
 );
-
-CREATE TABLE ProductosCategorias (
-    IdCategoria INT,
-    IdProducto INT,
-    CONSTRAINT PK_ProductosCategorias PRIMARY KEY (IdCategoria, IdProducto),
-    CONSTRAINT FK_ProductosCategorias_Categoria FOREIGN KEY (IdCategoria) REFERENCES Categorias(IdCategoria),
-    CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto)
-);
-GO
-
-CREATE PROCEDURE productosPorPedido 
-    @idPedido NVARCHAR(255)  -- Correct syntax for parameter type
-AS
-BEGIN
-    SELECT * 
-    FROM Productos P
-    INNER JOIN PedidosProductos PP ON PP.IdProducto = P.IdProducto
-    WHERE PP.IdPedido = @idPedido;
-END;
-
 INSERT INTO Proveedores (Nombre, Correo, Telefono, Direccion, Pais)
 VALUES 
     ('Alimentos Naturales S.A.', 'contacto@alimentosnaturales.com', '555-314-6721', 'Avenida Verde 42, Barrio Ecología, Ciudad Verde', 'México'),
@@ -111,6 +72,3 @@ VALUES
     ('Ropa de Cama 3 Piezas', 75.99, 6), -- Hogar y Cocina
     ('Cargador Solar', 25.99, 1), -- Tecnología
     ('Proyector Portátil', 210.50, 1); -- Tecnología
-
-
---exec productosPorPedido
