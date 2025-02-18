626d0d431a9a1dea0564f599587988c7e1a30333
eduarnesto <145675429+eduarnesto@users.noreply.github.com>
2025-02-18 13:12:41 +0100

    Correcion de PK

--- CreaccionBD.sql
+++ CreaccionBD.sql
@@ -1,9 +1,19 @@
+Create database Fernexus
+Use Fernexus
+
+CREATE TABLE Pedidos (
+    IdPedido INT IDENTITY(1,1),
+    FechaPedido DateTime NOT NULL,
+    Coste DECIMAL(10,2),
+	CONSTRAINT PK_Pedidos PRIMARY KEY (IdPedido)
+);
+
 CREATE TABLE Productos (
     IdProducto INT IDENTITY(1,1),
     Nombre VARCHAR(255) NOT NULL,
     Precio DECIMAL(10,2) NOT NULL,
     IdCategoria INT,
-	CONSTRAINT PK_Productos PRIMARY KEY (IdPedido),
+	CONSTRAINT PK_Productos PRIMARY KEY (IdProducto),
 );
 
 
@@ -17,24 +27,17 @@ CREATE TABLE Proveedores (
 	CONSTRAINT PK_Proveedores PRIMARY KEY (IdProveedor)
 );
 
-CREATE TABLE Pedidos (
-    IdPedido INT IDENTITY(1,1),
-    FechaPedido DateTime NOT NULL,
-    Coste DECIMAL(10,2),
-	CONSTRAINT PK_Pedidos PRIMARY KEY (IdPedido)
-);
-
 CREATE TABLE Categorias (
     IdCategoria INT IDENTITY(1,1),
     nombre VARCHAR(255) NOT NULL,
-	CONSTRAINT PK_Pedidos PRIMARY KEY (IdCategoria)
+	CONSTRAINT PK_Categorias PRIMARY KEY (IdCategoria)
 );
 
 CREATE TABLE PedidosProductos (
     IdPedido INT,
     IdProducto INT,
     Cantidad INT NOT NULL,
-    CONSTRAINT PK_Pedidos PRIMARY KEY (IdPedido, IdProducto),
+    CONSTRAINT PK_PedidosProductos PRIMARY KEY (IdPedido, IdProducto),
 	CONSTRAINT FK_PedidosProductos_Pedido FOREIGN KEY (IdPedido) REFERENCES Pedidos(IdPedido),
     CONSTRAINT FK_PedidosProductos_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto) 
 );
