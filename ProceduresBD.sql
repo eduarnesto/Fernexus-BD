2b23939b67c0a7ec993126c7f63f471cbe5ffcfa
eduarnesto <145675429+eduarnesto@users.noreply.github.com>
2025-03-04 13:19:34 +0100

    Cambios en filtrarPedidosPorFechas

--- ProceduresBD.sql
+++ ProceduresBD.sql
@@ -15,10 +15,32 @@ CREATE OR ALTER PROCEDURE filtrarPedidosPorFechas
     @fechaFin DATETIME
 AS
 BEGIN
-    SELECT *
-    FROM Pedidos
+    SELECT 
+        p.IdPedido, 
+        p.FechaPedido, 
+        pp.IdProducto, 
+        pp.IdProveedor,
+        pr.Nombre, 
+        pc.IdCategoria,
+        pp.Cantidad,
+        prp.PrecioUnidad * pp.Cantidad as PrecioTotal
+    FROM 
+        Pedidos p
+    JOIN 
+        PedidosProductos pp ON p.IdPedido = pp.IdPedido
+    JOIN 
+        Productos pr ON pp.IdProducto = pr.IdProducto
+    JOIN 
+        ProductosCategorias pc ON pr.IdProducto = pc.IdProducto
+    JOIN 
+        Categorias c ON pc.IdCategoria = c.IdCategoria
+    JOIN
+        ProveedoresProductos prp ON prp.IdProducto = pp.IdProducto AND prp.IdProveedor = p.IdProveedor
     WHERE FechaPedido BETWEEN @fechaInicio AND @fechaFin
-      AND deletedat = '1111-11-11';
+        AND p.deletedat = '1111-11-11'
+        AND pp.deletedat = '1111-11-11'
+        AND pr.deletedat = '1111-11-11'
+        AND pc.deletedat = '1111-11-11';
 END;
 
 CREATE OR ALTER PROCEDURE filtrarProductosPorCategoria
