19f81870533998c7e94498e9242e2848cd2f340f
eduarnesto <145675429+eduarnesto@users.noreply.github.com>
2025-03-04 13:09:38 +0100

    Cambios en el procedure PedidosProductos

--- CreacionBD.sql
+++ CreacionBD.sql
@@ -5,7 +5,7 @@ CREATE TABLE ProductosCategorias (
     IdProducto INT,
     Stock INT,
     Precio FLOAT NOT NULL,
-    DeletedAt DATE NULL,
+    DeletedAt DATE DEFAULT '1111-11-11',
     CONSTRAINT PK_ProductosCategorias PRIMARY KEY (IdCategoria, IdProducto, DeletedAt),
     CONSTRAINT FK_ProductosCategorias_Categoria FOREIGN KEY (IdCategoria) REFERENCES Categorias(IdCategoria),
     CONSTRAINT FK_ProductosCategorias_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto)
@@ -16,7 +16,7 @@ CREATE TABLE Pedidos (
     FechaPedido DATETIME NOT NULL,
     Coste DECIMAL(10,2),
     IdProveedor INT,
-    DeletedAt DATE NULL,
+    DeletedAt DATE DEFAULT '1111-11-11',
     CONSTRAINT PK_Pedidos PRIMARY KEY (IdPedido, DeletedAt),
     CONSTRAINT FK_Pedidos_Proveedores FOREIGN KEY (IdProveedor) REFERENCES Proveedores(IdProveedor)
 );
@@ -24,7 +24,7 @@ CREATE TABLE Pedidos (
 CREATE TABLE Productos (
     IdProducto INT IDENTITY(1,1),
     Nombre VARCHAR(255) NOT NULL,
-    DeletedAt DATE NULL,
+    DeletedAt DATE DEFAULT '1111-11-11',
     CONSTRAINT PK_Productos PRIMARY KEY (IdProducto, DeletedAt)
 );
 
@@ -35,14 +35,14 @@ CREATE TABLE Proveedores (
     Telefono VARCHAR(20) NOT NULL,
     Direccion TEXT NOT NULL,
     Pais VARCHAR(100) NOT NULL,
-    DeletedAt DATE NULL,
+    DeletedAt DATE DEFAULT '1111-11-11',
     CONSTRAINT PK_Proveedores PRIMARY KEY (IdProveedor, DeletedAt)
 );
 
 CREATE TABLE Categorias (
     IdCategoria INT IDENTITY(1,1),
     Nombre VARCHAR(255) NOT NULL,
-    DeletedAt DATE NULL,
+    DeletedAt DATE DEFAULT '1111-11-11',
     CONSTRAINT PK_Categorias PRIMARY KEY (IdCategoria, DeletedAt)
 );
 
@@ -50,7 +50,7 @@ CREATE TABLE PedidosProductos (
     IdPedido INT,
     IdProducto INT,
     Cantidad INT,
-    DeletedAt DATE NULL,
+    DeletedAt DATE DEFAULT '1111-11-11',
     PRIMARY KEY (IdPedido, IdProducto, DeletedAt),
     CONSTRAINT FK_PedidosProductos_Pedido FOREIGN KEY (IdPedido) REFERENCES Pedidos(IdPedido),
     CONSTRAINT FK_PedidosProductos_Producto FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto) 
@@ -60,7 +60,7 @@ CREATE TABLE ProveedoresProductos (
     IdProveedor INT,
     IdProducto INT,
     PrecioUnidad DECIMAL(10,2),
-    DeletedAt DATE NULL,
+    DeletedAt DATE DEFAULT '1111-11-11',
     CONSTRAINT PK_ProveedoresProductos PRIMARY KEY (IdProveedor, IdProducto, DeletedAt),
     CONSTRAINT FK_ProveedoresProductos_Proveedores FOREIGN KEY (IdProveedor) REFERENCES Proveedores(IdProveedor),
     CONSTRAINT FK_ProveedoresProductos_Productos FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto)
